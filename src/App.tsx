f97d6e80a43a0ce9697fe6ed981b170798da7def
femicr8v <femicr8v@gmail.com>
2025-07-29 08:56:35 +0100

    set up enviroment

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+
+      <h2 className="text-2xl font-bold">Hello World</h2>
+    </div>
+  );
+}
+
+export default App;
